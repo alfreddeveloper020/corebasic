095fd7441c9b11da79405b24f41f8ded53475ab4
alfreddeveloper020 <alfrenovembre020@gmail.com>
2019-11-04 18:43:00 -0600

    segundo commit

---
+++ CoreBasico/Data/CoreBasicoContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreBasico.Models
+{
+    public class CoreBasicoContext : DbContext
+    {
+        public CoreBasicoContext (DbContextOptions<CoreBasicoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CoreBasico.Models.Usuarios> Usuarios { get; set; }
+    }
+}
---
+++ CoreBasico/Models/Usuarios.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBasico.Models
+{
+    public class Usuarios
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string clave { get; set; }
+        public Nullable<int> estado { get; set; }
+        public string genero { get; set; }
+        public Nullable<int> edad { get; set; }
+        public string ciudad { get; set; }
+    }
+}
